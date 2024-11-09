445e22414d25f0d69f9c4ea7bb995aab31068102
Irokanade <michaeljs.leong@gmail.com>
2024-11-09 14:34:57 +0800

    fix: favicon

--- src/App.vue
+++ src/App.vue
@@ -12,25 +12,3 @@
 import NavBar from "./components/NavBar.vue";
 // import FooterComp from "./components/FooterComp.vue";
 </script>
-
-<style scoped>
-/* Apply padding globally for the entire app */
-.v-application {
-  padding: 20px; /* Adjust as needed */
-}
-
-/* Optionally add padding to the main content */
-.app-main {
-  padding-top: 20px; /* Adjust the padding as necessary */
-  padding-bottom: 20px;
-}
-
-/* Optional: Add padding for NavBar and Footer if needed */
-.NavBar {
-  padding: 10px; /* Adjust as needed */
-}
-
-.FooterComp {
-  padding: 10px; /* Adjust as needed */
-}
-</style>
