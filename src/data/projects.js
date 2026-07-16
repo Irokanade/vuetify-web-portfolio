bd7e873532682a4531a709be487c7fa1533c6a17
Irokanade <michaeljs.leong@gmail.com>
2026-07-16 17:44:13 +0800

    chore: update projects

--- src/data/projects.js
+++ src/data/projects.js
@@ -12,6 +12,20 @@ export const projects = [
     description: `This is a chess engine with a graphical user interface (GUI) that allows users to play chess against the engine. The engine uses a minimax algorithm to determine the best move for the player.`,
   },
   {
+    id: "iro-cache-simulator",
+    title: "CPU Cache Simulator",
+    image: null,
+    link: "https://github.com/Irokanade/iro-cache-simulator",
+    description: `Cachegrind inspired user space header only cpu cache simulator to count cache hits and cache miss of a program. This implementation uses C++23 and uses cpu_read and cpu_write functions directly in source code to count.`,
+  },
+  {
+    id: "measure-l1-cache-latency",
+    title: "Measure L1 Cache Latency",
+    image: null,
+    link: "https://github.com/Irokanade/measure-l1-cache-latency",
+    description: `Simple timing test for cold reading into L1 cache by evicting the data all the way to L2 cache then read it again. Repeat the timing 20,000 times and subtract the cache eviction timing then calculate the average time to read the data.`,
+  },
+  {
     id: "software-engineering-course",
     title: "Software Engineering Course",
     image: softEngProject,
@@ -26,7 +40,7 @@ export const projects = [
   {
     id: "portfolio-website",
     title: "Portfolio Website",
-    image: "null",
+    image: null,
     link: "https://github.com/Irokanade/vuetify-web-portfolio",
     description: `This is the website hope you like it :)`,
   },
